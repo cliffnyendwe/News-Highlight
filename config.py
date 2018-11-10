bf6b2a7558745a51dc2e68b7f1f568719c60221b
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-11-10 13:13:08 +0300

    added column to index.html

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
   NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
   NEWS_API_SECONDARY_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
   NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-  SECRET_KEY = os.environ.get('SECRET_KEY')
+  # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProdConfig(Config):
